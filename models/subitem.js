c7f3c3f4da6c15146251be18c68e1d25f0c187a2
armi1189 <armi1189@gmail.com>
2015-05-15 10:52:30 +0100

    fresh start

--- models/subitem.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var SubItemSchema = new mongoose.Schema({
-  name: String,
-  status: Boolean,
-  updated_at: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('SubItem', SubItemSchema);
